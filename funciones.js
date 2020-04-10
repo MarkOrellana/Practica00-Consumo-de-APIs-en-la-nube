824cfa6dddb5119ac610f0fd4c41105056880f75
Daniel Carpio <markore2013@gmail.com>
2020-04-10 15:44:36 -0500

    Incluyendo informacion de movies

--- funciones.js
+++ funciones.js
@@ -2,37 +2,35 @@
 function cargarDatos(txto) {
     console.log(txto);
     var detalles = "";
-    if (txto == "") {
-        detalles += "<h1>No Informacion Disponible</h1>";
-        document.getElementById('answer').innerHTML = detalles;
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
     } else {
-        if (window.XMLHttpRequest) {
-            xmlhttp = new XMLHttpRequest();
-        } else {
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange = function () {
-
-            if (this.readyState == 4 && this.status == 200) {
-                var data = JSON.parse(this.responseText)
-                data.Search.forEach(movie => {
-                    //console.log(movie.imdbID)
-                    detalles +=
-                "<div class=modal-dialog >"+
-                "<div class=modal-content md>"+
-                "<div class=modal-body><h1>"+
-                movie.Title+"</h1><br>"+
-                "<img class=card-img-top src='"+movie.Poster+"'><br>"+
-                "<p class=card-text> Year Released: "+movie.Year+"</p>"+
-                "<p class=card-text> Type: "+movie.Type+"</p>"+
-                "<a class=btn btn-primary btn-lg btn purple-gradient data-toggle=modal href='#dat' onclick=\"buscaId('"+movie.imdbID+"')\">Mas Detalles</a>"+"</div>"+"</div>"+"</div>";
-                 });
-                document.querySelector('#answer').innerHTML = detalles;
-            }
-        };
-        xmlhttp.open("GET", "http://www.omdbapi.com/?s=" + txto + "&apikey=86584c34", true);
-        xmlhttp.send();
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
+    xmlhttp.onreadystatechange = function () {
+
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText)
+            data.Search.forEach(movie => {
+                //console.log(movie.imdbID)
+                detalles +=
+                    "<div class=modal-dialog >" +
+                    "<div class=modal-content md>" +
+                    "<div class=modal-body><h1>" +
+                    movie.Title + "</h1><br>" +
+                    "<img class=card-img-top src='" + movie.Poster + "'><br>" +
+                    "<p class=card-text> Year Released: " + movie.Year + "</p>" +
+                    "<p class=card-text> Type: " + movie.Type + "</p>" +
+                    "<a class=btn btn-primary btn-lg btn purple-gradient data-toggle=modal href='#dat' onclick=\"buscaId('" + movie.imdbID + "')\">Mas Detalles</a>" + "</div>" + "</div>" + "</div>";
+            });
+            document.getElementById('answer').innerHTML = detalles;
+        } else {
+            document.getElementById('answer').innerHTML = "<h2> No se a encontrado resultados. </h2>"
+        }
+    };
+    xmlhttp.open("GET", "http://www.omdbapi.com/?s=" + txto + "&apikey=86584c34", true);
+    xmlhttp.send();
+
 }
 function buscaId(id) {
     console.log(id);
@@ -46,41 +44,35 @@ function buscaId(id) {
         if (this.readyState == 4 && this.status == 200) {
             const peli = JSON.parse(this.responseText)
             console.log(peli);
-            deta += `
-            <div class="modal fade ">
-            <div class="modal-dialog ">
-                <div class="modal-content md">
-                    <div class="modal-header">
+            deta = `
+            <div class="modal-dialog">
+            <div class="modal-content md">
+                <div class="modal-header">
                     <h4 class=modal-title>Title:${peli.Title}</h4>
-                    </div>
-                    <div class="modal-body">
-                    "<p class=card-text>Year: "${peli.Year}"</p>"
-                    "<p class=card-text>Released: "${peli.Released}"</p>"
-                    "<p class=card-text>RunTime: "${peli.Runtime}"</p>"
-                    "<p class=card-text>Genre: "${peli.Genre}"</p>"
+                </div>
+                <div class="modal-body">
+                <p class=card-text>Year: ${peli.Year}</p>
+                <p class=card-text>Released: ${peli.Released}</p>
+                <p class=card-text>RunTime: ${peli.Runtime}</p>
+                <p class=card-text>Genre: ${peli.Genre}</p>
+                <p class=card-text>Director: ${peli.Director}</p>
+                <p class=card-text>Writer: ${peli.Writer}</p>
+                <p class=card-text>Actors: ${peli.Actors}</p>
+                <p class=card-text>Plot: ${peli.Plot}</p>
+                <p class=card-text>Language: ${peli.Language}</p>
+                <p class=card-text>Country: ${peli.Country}</p>
+                <p class=card-text>Awards: ${peli.Awards}</p>
+                <p class=card-text>Metascore: ${peli.Metascore}</p>
+                <p class=card-text>imdbRating: ${peli.imdbRating}</p>
+                <p class=card-text>DVD: ${peli.DVD}</p>
+                <p class=card-text>imdbVotes: ${peli.imdbVotes}</p>
 
-                    </div>
                 </div>
             </div>
-        </div>
-        `;
-            // "<div class=modal-dialog><div class=modal-content md><div class=moda-header>"+
-            // "<h4 class=modal-title>Title: "+peli.Title+"</h4>"+
-            // "</div>"+
-            // "<div class=modal-body>"+
-            // "<p class=card-text>Year: "+peli.Year+"</p>"+
-            // "<p class=card-text>Rated: "+peli.Rated+"</p>"+
-            // "<p class=card-text>Released: "+peli.Released+"</p>"+
-            // "<p class=card-text>Runtime: "+peli.Runtime+"</p>"+
-            // "<p class=card-text>Genre: "+peli.Genre+"</p>"+
-            // "<p class=card-text>Director: "+peli.Director+"</p>"+
-            // "<p class=card-text>Writer: "+peli.Writer+"</p>"+
-            // "<p class=card-text>Rated: "+peli.Actors+"</p>"+
-            // "<p class=card-text>Awards: "+peli.Awards+"</p>"+
-            // "</div></div></div>";
+        </div>`;
             console.log(deta);
 
-            document.querySelector('#dat').innerHTML = deta
+            document.getElementById("dat").innerHTML = deta
         }
     };
     xmlhttp.open("GET", "http://www.omdbapi.com/?apikey=86584c34&i=" + id, true);
